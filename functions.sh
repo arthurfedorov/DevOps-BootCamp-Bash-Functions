821a4cc8ff1e11b0d6a712ed07241dc5a15eb736
Arthur Fedorov <fedorov.arthur@gmail.com>
2022-12-12 16:00:35 +0400

    Task completed

--- functions.sh
+++ functions.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 # Put your code here
+
+pow(){
+    let result="$1 ** $2"
+    echo $result
+}
+
+print_log() {
+  log_date=$(date "+%Y-%m-%d %H:%M")
+  echo "[$log_date] $1"
+}
+
+shortest(){
+    for i in 
+}
